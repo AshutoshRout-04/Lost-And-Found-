467a5abaa7647133161344f9b40d48e22657d56a
Ashutosh Rout-04 <ashutoshroutas04@gmail.com>
2026-08-17 20:32:31 +0530

    test

--- BACKEND/lost_and_found_v1/src/main/java/com/lostnfound/lost_and_found_v1/item/entity/Item.java
+++ BACKEND/lost_and_found_v1/src/main/java/com/lostnfound/lost_and_found_v1/item/entity/Item.java
@@ -30,7 +30,6 @@ public class Item {
     private String description;
     private String location;
     private LocalDate date;
-    //gccuehcue
     @Enumerated(EnumType.STRING)
     private ItemType itemType;
 
